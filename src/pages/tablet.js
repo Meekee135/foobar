b465f6efd3ada565efb0ba6b637f0d6a33053130
Miko <kozak.mikolaj@gmail.com>
2021-06-25 16:19:09 +0200

    hope this fix

--- src/pages/tablet.js
+++ src/pages/tablet.js
@@ -197,7 +197,7 @@ const Tablet = () => {
   return (
     // <div className={`Grid_Container ${theme ? "Dark_Theme" : "Light_Theme"}`}>
     <div className="Grid_Container Dark_Theme">
-      {/* <div className="Position_Grid" /> */}
+      <div className="Position_Grid" />
       <Guests
         focusing={(e) => {
           let index = e.target.dataset.index;
