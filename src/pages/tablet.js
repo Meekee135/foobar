53734c9a74258aae2f7572a29f94f0e0e782a782
Miko <kozak.mikolaj@gmail.com>
2021-06-25 17:14:24 +0200

    x

--- src/pages/tablet.js
+++ src/pages/tablet.js
@@ -22,13 +22,9 @@ const Tablet = () => {
   let missing = filled.filter((el) => !payments.includes(el));
   useEffect(() => {
     let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
     window.addEventListener('resize', () => {
       document.documentElement.style.setProperty('--vh', `${vh}px`)
-    })
-    window.addEventListener('onload', () => {
-      setTimeout(() => {
-        document.documentElement.style.setProperty('--vh', `${vh}px`)
-      }, 500);
     })
   })
   useEffect(() => {
